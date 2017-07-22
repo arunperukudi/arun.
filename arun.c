b25d41c5c10f76fa35f77b52a122533448de75da
arunperukudi <arunperukudi@gmail.com>
2017-07-22 18:01:42 +0530

    Create arun.c

---
+++ arun.c
@@ -0,0 +1,14 @@
+#included<stdio.h>
+int main()
+{
+int a=0;
+printf("enter the value of a");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("a is even number");
+else
+{
+printf("a is odd number");
+} return 0;
+}
